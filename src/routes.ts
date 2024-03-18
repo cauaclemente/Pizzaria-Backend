3a43b52b624b6bb57a04e060eb349476d6a0375a
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-18 17:35:28 -0300

    Logando e gerando Token JWT

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/users/CreateUserController";
-
+import { AuthUserController } from "./controllers/users/AuthUserController";
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
+
+router.post('/session', new AuthUserController().handle)
+
+
 
 export {router}
