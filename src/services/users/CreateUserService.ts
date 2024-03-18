92765578dda4f93d8b5b579ab145aec44e171ad1
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-18 15:58:04 -0300

    Criptografando a senha do usuario

--- src/services/users/CreateUserService.ts
+++ src/services/users/CreateUserService.ts
@@ -1,5 +1,5 @@
 import prismaClient from "../../prisma";
-
+import { hash } from "bcryptjs";
 
 interface UserRequest{
     name: string;
@@ -24,11 +24,13 @@ class CreateUserService {
             throw new Error("Esse email ja existe")
         }
 
+        const passwordHash = await hash(password, 8)
+
         const user = await prismaClient.user.create({
             data: {
                 name: name,
                 email: email,
-                password: password,
+                password: passwordHash,
             },
             select: {
                 id: true,
