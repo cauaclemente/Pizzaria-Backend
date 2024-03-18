4bd1cd7dc26b9c5a58e35b3cd4e989916d2e7091
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-18 20:33:11 -0300

    Listando categorias

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateUserController } from "./controllers/users/CreateUserController";
 import { AuthUserController } from "./controllers/users/AuthUserController";
 import { DetailUserController } from "./controllers/users/DetailUserController";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -16,5 +17,6 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export {router}
