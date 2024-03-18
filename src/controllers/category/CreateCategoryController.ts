908d6c517ebac743e62e03b1027dd55cf22a1457
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-18 20:17:31 -0300

    Cadastrando categorias

---
+++ src/controllers/category/CreateCategoryController.ts
@@ -0,0 +1,19 @@
+import { Response, Request } from "express";
+import { CreateCategoryService } from "../../services/Category/CreateCategoryService";
+
+class CreateCategoryController{
+    async handle(req: Request, res: Response){
+        const { name } = req.body
+
+        const createCategoryService = new CreateCategoryService();
+
+        const category =  await createCategoryService.execute({
+            name,
+        })
+
+        return res.json( category ) 
+
+    }
+}
+
+export { CreateCategoryController}
--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/users/CreateUserController";
 import { AuthUserController } from "./controllers/users/AuthUserController";
 import { DetailUserController } from "./controllers/users/DetailUserController";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 
@@ -11,7 +12,9 @@ router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
 
-router.get("/me", isAuthenticated, new DetailUserController().handle)
+router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 
 export {router}
