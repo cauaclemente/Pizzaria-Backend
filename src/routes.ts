0be2d4b9ec0aae32a138866d86aea3c7125bbe12
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-19 20:04:19 -0300

    Removendo item da mesa

--- src/routes.ts
+++ src/routes.ts
@@ -13,7 +13,9 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+
 import { AddItemController } from "./controllers/order/AddItemController";
+import { RemoveItemController } from "./controllers/order/RemoveItemController";
 
 import upploadConfig from "./config/multer"
 
@@ -45,5 +47,7 @@ router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+
 
 export {router}
