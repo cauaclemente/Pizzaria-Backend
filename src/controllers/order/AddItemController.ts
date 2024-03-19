909cfad751ce906080bed06c26ffade855f03ff3
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-19 19:43:39 -0300

    Adicionando itens a mesa

---
+++ src/controllers/order/AddItemController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { AddItemService } from "../../services/order/AddItemService";
+
+class AddItemController{
+    async handle(req: Request, res: Response){
+        const { order_id, amount, product_id} = req.body;
+
+        const AddItem = new AddItemService();
+
+        const order = await AddItem.execute({
+            order_id,
+            product_id,
+            amount
+        })
+
+        return res.json(order)
+    }
+}
+
+export { AddItemController }
--- src/routes.ts
+++ src/routes.ts
@@ -13,6 +13,7 @@ import { ListByCategoryController } from "./controllers/product/ListByCategoryCo
 
 import { CreateOrderController } from "./controllers/order/CreateOrderController";
 import { RemoveOrderController } from "./controllers/order/RemoveOrderController";
+import { AddItemController } from "./controllers/order/AddItemController";
 
 import upploadConfig from "./config/multer"
 
@@ -42,5 +43,7 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 // -- ROTAS ORDER --
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
 
 export {router}
