ffacf33721b4b0e97d060a775777360cafc16ae5
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-19 17:24:44 -0300

    Filtrando produtos de uma categoria

--- src/routes.ts
+++ src/routes.ts
@@ -9,6 +9,7 @@ import { CreateCategoryController } from "./controllers/category/CreateCategoryC
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
 
 import { CreateProductController } from "./controllers/product/CreateProductController";
+import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
 import upploadConfig from "./config/multer"
 
@@ -18,16 +19,22 @@ const router = Router();
 
 const upload = multer(upploadConfig.upload("./tmp"))
 
+// -- ROTAS USER --
 router.post('/users', new CreateUserController().handle)
 
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
 
+// -- ROTAS CATEGORY --
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
+// -- ROTAS PRODUCT --
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
+
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
 
 export {router}
