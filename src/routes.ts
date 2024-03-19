5e158f2108b52dd7cc0c1c144475a100ac4c21e2
Cauã Clemente <cauaclemente07@gmail.com>
2024-03-19 18:00:01 -0300

    Cadastrando pedidos

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,8 @@ import { ListCategoryController } from "./controllers/category/ListCategoryContr
 import { CreateProductController } from "./controllers/product/CreateProductController";
 import { ListByCategoryController } from "./controllers/product/ListByCategoryController";
 
+import { CreateOrderController } from "./controllers/order/CreateOrderController";
+
 import upploadConfig from "./config/multer"
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
@@ -35,6 +37,9 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
 router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
+
+// -- ROTAS ORDER --
+router.post('/order', isAuthenticated, new CreateOrderController().handle)
 
 
 export {router}
